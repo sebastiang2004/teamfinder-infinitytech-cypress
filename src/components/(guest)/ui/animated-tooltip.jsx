0b75b234ebb33ff8d722d7362ba10aa8b704c310
Marina <iriciucmarina@yahoo.com>
2024-03-18 12:21:34 +0200

    Removed duplicate alt attribute

--- src/components/(guest)/ui/animated-tooltip.jsx
+++ src/components/(guest)/ui/animated-tooltip.jsx
@@ -72,7 +72,7 @@ export const AnimatedTooltip = ({
           </AnimatePresence>
           <img src={item.image} onMouseMove={handleMouseMove} alt={item.name} height={100} width={100}
                className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
-               alt=""/>
+               />
         </div>
       ))}
     </>
